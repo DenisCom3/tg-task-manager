ff016a0bcae00ea8426d9249eaf6ad944b6746c5
DenisCom3 <denis.chernov.and@gmail.com>
2024-04-08 00:43:39 +0400

    init config

--- cmd/time-manager/main.go
+++ cmd/time-manager/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time-manager/internal/config"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("Hello, Go!")
+	err := run()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+func run() error {
+	
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return err
+	}
+
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg.Env)
+
+	return nil
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 run:
-	go run cmd/tima-manager/main.go
+	go run cmd/time-manager/main.go
